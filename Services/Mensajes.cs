133df70db1e106d06ac96c3a4e7107ca46b12d70
ArianaST <marinestrellita@hotmail.com>
2022-02-24 22:15:32 +0000

    soap-test docker

---
+++ Services/Mensajes.cs
@@ -0,0 +1,25 @@
+using System;
+
+using System.ServiceModel;
+
+
+
+namespace WSDL.mensajes{
+
+    [ServiceContract]
+
+    public interface Mensajes
+
+    {
+
+        [OperationContract]
+
+        string Saludar(string nombre);
+
+        [OperationContract]
+
+        string Mostrar (int id);
+
+    }
+
+}
